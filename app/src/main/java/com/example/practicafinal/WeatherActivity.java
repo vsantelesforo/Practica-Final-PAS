b30716548abb240215d9ad64d72c5ace99a42cfd
Victor San Telesforo <victor.santelesforo.garcia@alumnos.upm.es>
2022-07-12 19:17:27 +0200

    Add Humedad y Precipitaciones

--- app/src/main/java/com/example/practicafinal/WeatherActivity.java
+++ app/src/main/java/com/example/practicafinal/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.practicafinal;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -18,58 +19,56 @@ import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class WeatherActivity extends AppCompatActivity {
 
-    // private JsonObjectRequest request;
     LocationManager locationManager;
     Boolean location_allowed;
+    Date time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        setTitle("Meteo");
+
         RequestQueue queue = Volley.newRequestQueue(WeatherActivity.this);
 
-        final TextView textView = findViewById(R.id.textView_rest);
+        final TextView textView_temperature = findViewById(R.id.textView_temp);
+        final TextView textView_humidity = findViewById(R.id.textView_hum);
+        final TextView textView_precipitation = findViewById(R.id.textView_prec);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         location_allowed = (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED);
 
         String [] coordinates = getLocationValues();
-        // String api_url = "https://api.open-meteo.com/v1/forecast?latitude=40.4167&longitude=-3.7033&hourly=temperature_2m";
         String api_url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" +
-                coordinates[0] + "&longitude=" + coordinates[1] + "&hourly=temperature_2m";
-
-//        StringRequest stringRequest = new StringRequest(Request.Method.GET, api_url,
-//                new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        try {
-//                            JSONObject json = new JSON(response.toString());
-//                            textView.setText("Response is: " + json.toString());
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                    }, new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        textView.setText("Didn't work");
-//                    }
-//                });
+                coordinates[0] + "&longitude=" + coordinates[1] + "&hourly=temperature_2m,relativehumidity_2m,precipitation";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, api_url, null, new Response.Listener<JSONObject>() {
 
+                    @RequiresApi(api = Build.VERSION_CODES.O)
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
                             String temperatures = new JSONObject(response.get("hourly").toString()).getString("temperature_2m");
+                            String humidities = new JSONObject(response.get("hourly").toString()).getString("relativehumidity_2m");
+                            String precipitacions = new JSONObject(response.get("hourly").toString()).getString("precipitation");
                             String [] temperature = temperatures.split(",");
-                            textView.setText("Response: " + temperature[12]);
+                            String [] humidity = humidities.split(",");
+                            String [] precipitation = precipitacions.split(",");
+                            time = Calendar.getInstance().getTime();
+                            int hora_actual = time.getHours();
+                            textView_temperature.setText(temperature[hora_actual]);
+                            textView_humidity.setText((String.format("%s %%", humidity[hora_actual])));
+                            textView_precipitation.setText(String.format("%s %%", precipitation[hora_actual]));
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -82,9 +81,6 @@ public class WeatherActivity extends AppCompatActivity {
 
                     }
                 });
-
-        // Access the RequestQueue through your singleton class.
-
 
         queue.add(jsonObjectRequest);
     }
