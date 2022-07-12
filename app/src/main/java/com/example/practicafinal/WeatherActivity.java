0131c6d529190bcd46bf099e33291ea8fd050ba5
vsantelesforo <victorsantelesforo@gmail.com>
2022-07-12 15:51:53 +0200

    Obtener coordenadas del movil funcionando

--- app/src/main/java/com/example/practicafinal/WeatherActivity.java
+++ app/src/main/java/com/example/practicafinal/WeatherActivity.java
@@ -1,27 +1,31 @@
 package com.example.practicafinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import java.lang.reflect.Array;
 
 public class WeatherActivity extends AppCompatActivity {
 
     // private JsonObjectRequest request;
+    LocationManager locationManager;
+    Boolean location_allowed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +35,13 @@ public class WeatherActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(WeatherActivity.this);
 
         final TextView textView = findViewById(R.id.textView_rest);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        location_allowed = (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED);
 
-        String api_url = "https://api.open-meteo.com/v1/forecast?latitude=40.4167&longitude=-3.7033&hourly=temperature_2m";
+        String [] coordinates = getLocationValues();
+        // String api_url = "https://api.open-meteo.com/v1/forecast?latitude=40.4167&longitude=-3.7033&hourly=temperature_2m";
+        String api_url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" +
+                coordinates[0] + "&longitude=" + coordinates[1] + "&hourly=temperature_2m";
 
 //        StringRequest stringRequest = new StringRequest(Request.Method.GET, api_url,
 //                new Response.Listener<String>() {
@@ -78,5 +87,15 @@ public class WeatherActivity extends AppCompatActivity {
 
 
         queue.add(jsonObjectRequest);
+    }
+
+    @SuppressLint("MissingPermission")
+    public String [] getLocationValues(){
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) && location_allowed){
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            return new String []{String.valueOf(location.getLatitude()), String.valueOf(location.getLongitude())};
+        }else {
+            return new String [] {"40", "-4"}; // Madrid por defecto
+        }
     }
 }
